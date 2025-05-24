8f555306103c46065cb10bef7d6c9a642c50e5cb
rishabh2916 <rishabhrajpurohit1703@gtmail.com>
2025-05-24 09:11:13 +0530

    variable Exercise

---
+++ practiceSet/javaScript_Basic.js
@@ -0,0 +1,11 @@
+// alert("rishabh rajpurohit");
+// prompt("what is your name")
+
+// console.log("rishabh rajpurohit");
+
+//calculation with variables
+const tShirt = 10;
+const socks = 8;
+const dinnerPlates = 200;
+const total = tShirt + socks + dinnerPlates;
+console.log(total);
