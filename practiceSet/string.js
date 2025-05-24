dbe60281ac06e747357e24734ebfd2255673918d
rishabh2916 <rishabhrajpurohit1703@gtmail.com>
2025-05-24 11:10:33 +0530

    String practice

---
+++ practiceSet/string.js
@@ -0,0 +1,44 @@
+const text = "My name is:";
+const name = "Rishabh Rajpurohit";
+
+//concatenation of string
+
+const concatenation = text + name;
+console.log(concatenation);
+
+const coffee = 500;
+const bagel = 300;
+const total =  Math.round((coffee + bagel)/100);
+const totalCostCoffee = "total cost of coffee $" + total;
+console.log(totalCostCoffee);
+
+//template string and interpolation
+
+const sentence = `${text} ${name}`;
+console.log(sentence);
+
+// challenging Exercises 
+const basketball = (2*2095) 
+const basketballShipping = 499;
+const tShirt = (2*799);
+const tShirtShipping = 499;
+
+const itemsCal = (basketball + tShirt) / 100;
+console.log(`$${itemsCal}`);
+
+const totalShipping = (basketballShipping + tShirtShipping) / 100;
+console.log(`$${totalShipping}`);
+
+const totalBeforeText = itemsCal + totalShipping;
+console.log(`Total before tax: $${totalBeforeText}`);
+
+const tax = totalBeforeText / 10;
+const rounded = parseFloat(tax.toFixed(2));
+console.log(`Estimated tax(10%): $${rounded}`);
+
+const totalPrice = itemsCal + totalShipping + totalBeforeText + rounded;
+console.log(`Order total: $${totalPrice}`);
+
+// basketball = (1*2095) 
+// tShirt = (1*799);
+
