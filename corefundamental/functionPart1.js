600469f159b5e5fb426877c9594573926e99b51f
rishabh2916 <rishabhrajpurohit1703@gtmail.com>
2025-05-26 07:11:32 +0530

    function part-2

--- corefundamental/functionPart1.js
+++ corefundamental/functionPart1.js
@@ -31,3 +31,32 @@ function loginUserMessage(userName) {
 
 console.log(loginUserMessage("rishabh"));
 console.log(loginUserMessage());
+
+function calculateCartPrice(val1, val2, ...num1){
+  return num1;
+}
+// ... it is rest operator and these is also spread operator
+console.log(calculateCartPrice(200, 400, 500, 600,))
+
+const user = {
+  userName: "RISHA",
+  price: 99
+}
+
+function handleObject(anyObject){
+  console.log(`UserName is ${anyObject.userName} and price is ${anyObject.price}`);
+}
+
+handleObject(user);
+handleObject({
+  userName:"sam",
+  price: 399
+})
+
+const myNewArray = [200, 300, 400, 500];
+function returnSecondValue(getArray){
+  return getArray[2]
+}
+
+// console.log(returnSecondValue(myNewArray));
+console.log(returnSecondValue([200, 300, 400, 500]));
