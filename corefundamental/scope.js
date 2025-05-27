5e9082e4303f89a47a01aa7b502983c0c4fccf5b
rishabh2916 <rishabhrajpurohit1703@gmail.com>
2025-05-27 16:20:50 +0530

    scope level and mini hosting

--- corefundamental/scope.js
+++ corefundamental/scope.js
@@ -5,7 +5,7 @@ if(true){
   // block scope
       let a = 10;
       const b = 20;
-      console.log("Inner: ", a)
+      // console.log("Inner: ", a)
       // var c = 30;
 }
 
@@ -13,3 +13,43 @@ console.log(a);
 // console.log(b);
 // console.log(c);
 
+
+function one() {
+  const username = "rishabh";
+
+  function two() {
+  const website = "youtube";
+  // console.log(username);
+}
+// console.log(website);
+
+// two(); 
+
+}
+// one();
+
+if(true) {
+  const username = "rishabh"
+  if(username === "rishabh") {
+    const website = " youtube";
+    // console.log(username + website)
+  }
+  // console.log(website);
+}
+
+// console.log(username);
+
+// +++++++++++++++++ intersting +++++++++++++++++++
+
+console.log(addone(5)); 
+
+function addone(num){
+  return num + 1;
+
+}
+
+const addTwo = function(num){
+  return num + 2
+}
+
+console.log(addTwo(5));
