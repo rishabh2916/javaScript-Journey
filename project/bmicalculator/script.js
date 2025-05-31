75cc57b0b666e75e4d06b9b350ed44a94a4b3e5b
rishabh2916 <rishabhrajpurohit1703@gmail.com>
2025-05-31 17:02:12 +0530

    Display the BMI weight Guide acc. to calculate

--- project/bmicalculator/script.js
+++ project/bmicalculator/script.js
@@ -15,18 +15,23 @@ form.addEventListener('submit', function (e) {
     results.innerHTML = `please give a valid height ${height}`;
   } else if (weight === '' || weight < 0 || isNaN(weight)) {
     results.innerHTML = `please give a valid weight ${weight}`;
-  } else {
-    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
-    results.innerHTML = `<span>${bmi}</span>`;
+  } 
+  
+  const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+  results.innerHTML = `<span>${bmi}</span>`;
 
-    if (bmi <= 18.6) {
-      underWeight.innerHTML = `ans => Under Weight 
-       less than 18.6`;
-    } else if (bmi > 18.6 || bmi < 24.9) {
-      normalRange.innerHTML = `ans => normal Weight 
-       18.6 - 24.9`;
-    } else if (bmi >= 24.9) {
-      overWeight.innerHTML = `ans => greater than 24.9`;
-    }
+  underWeight.style.display = 'none';
+  normalRange.style.display = 'none';
+  overWeight.style.display = 'none';
+
+  if (bmi < 18.6) {
+    underWeight.innerHTML = `Answer => Under Weight (less than 18.6)`;
+    underWeight.style.display = 'block';
+  } else if (bmi >= 18.6 && bmi <= 24.9) {
+    normalRange.innerHTML = `Answer => Normal Weight (18.6 - 24.9)`;
+    normalRange.style.display = 'block';
+  } else {
+    overWeight.innerHTML = `Answer => Over Weight (greater than 24.9)`;
+    overWeight.style.display = 'block';
   }
 });
